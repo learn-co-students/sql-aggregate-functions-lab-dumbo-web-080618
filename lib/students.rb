e4f5be194d7a20793e256f6f87a36c5a7a980949
mikepcarlos <michaelpcarlos@gmail.com>
2018-08-15 11:43:04 -0400

    SQL aggregate functions lab done

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,38 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
+#
+# CREATE TABLE students
+#     (id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     grade INTEGER,
+#     gpa FLOAT,
+#     tardies INTEGER);
 
 def highest_student_gpa
+  sql = <<-SQL
+  SELECT MAX(gpa) FROM students;
+  SQL
 end
 
 def lowest_student_gpa
+  sql = <<-SQL
+  SELECT MIN(gpa) FROM students;
+  SQL
 end
 
 def average_student_gpa
+  sql = <<-SQL
+  SELECT AVG(gpa) FROM students;
+  SQL
 end
 
 def total_tardies_for_all_students
+  sql = <<-SQL
+  SELECT SUM(tardies) FROM students;
+  SQL
 end
 
 def average_gpa_for_9th_grade
+  sql = <<-SQL
+  SELECT AVG(gpa) FROM students WHERE grade = 9;
+  SQL
 end
